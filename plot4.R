1b4d53cbd80842998e99f74f39d4e4e57f892fcf
yyongning <yyongning@gmail.com>
2017-08-27 21:56:50 +0800

    update homework

--- plot4.R
+++ plot4.R
@@ -10,13 +10,13 @@ work_data<-cbind(work_data, DTime)
 
 par(mfrow = c(2,2))
 
-with(work_data, plot(DTime, Global_active_power/1000, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
+with(work_data, plot(DTime, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
 with(work_data, plot(DTime, Voltage, type="l", ylab="Voltage", xlab="datetime"))
 with(work_data, plot(DTime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab=""))
 with(work_data, lines(DTime, Sub_metering_2, col = "red"))
 with(work_data, lines(DTime, Sub_metering_3, col = "blue"))
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c("black", "red", "blue"))
-with(work_data, plot(DTime, Global_active_power, type="l", ylab="Global_active_power", xlab="datetime"))
+with(work_data, plot(DTime, Global_reactive_power, type="l", ylab="Global_active_power", xlab="datetime"))
 
 dev.copy(png, "plot4.png", height=430, width=480)
 dev.off()
