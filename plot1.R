e3204df2f012e0980830341f87037812b251f73e
yyongning <yyongning@gmail.com>
2017-08-27 21:20:43 +0800

    update homework

--- plot1.R
+++ plot1.R
@@ -1,9 +1,7 @@
-all_data=read.table("household_power_consumption.txt", sep=";")
-work_data = subset(all_data, all_data$V1 == "1/2/2007" | all_data$V1 == "2/2/2007")
-names(work_data) = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+all_data <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", na="?", colClasses=c(rep("character", 2), rep("numeric", 7)))
 
-work_data$Global_active_power <- as.numeric(work_data$Global_active_power)
-hist(work_data$Global_active_power/1000, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+work_data = subset(all_data, all_data$Date == "1/2/2007" | all_data$Date == "2/2/2007")
+hist(work_data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 
 dev.copy(png, "plot1.png", height=430, width=480)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,13 +1,7 @@
-all_data=read.table("household_power_consumption.txt", sep=";")
-work_data = subset(all_data, all_data$V1 == "1/2/2007" | all_data$V1 == "2/2/2007")
-names(work_data) = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+all_data <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", na="?", colClasses=c(rep("character", 2), rep("numeric", 7)))
 
-work_data$Global_active_power <- as.numeric(work_data$Global_active_power)
-work_data$Global_reactive_power <- as.numeric(work_data$Global_reactive_power)
-work_data$Voltage <- as.numeric(work_data$Voltage)
-work_data$Sub_metering_1 <- as.numeric(work_data$Sub_metering_1)
-work_data$Sub_metering_2 <- as.numeric(work_data$Sub_metering_2)
-work_data$Sub_metering_3 <- as.numeric(work_data$Sub_metering_3)
+work_data = subset(all_data, all_data$Date == "1/2/2007" | all_data$Date == "2/2/2007")
+
 work_data$Date <- as.character(work_data$Date)
 work_data$Time <- as.character(work_data$Time)
 DTime<-paste(work_data$Date, work_data$Time)
